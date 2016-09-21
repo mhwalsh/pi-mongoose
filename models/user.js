bcd7f276f40d8376e499cf3a213253965f69a91b
Millie Walsh <millie@primeacademy.io>
2016-09-21 16:57:22 -0500

    added schema and db connection string

---
+++ models/user.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: String,
+  username: {type: String, unique: true},
+  admin: Boolean,
+  meta: {
+    age: Number,
+    website: String
+  },
+  created_at: Date
+});
+
+// users is the name of the collection in the database
+// WARNING will lowercase and pluralize collection name
+var User = mongoose.model('users', userSchema);
+
+module.exports = User;
--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
+
+//model
+var User = require('../models/user');
+
+//connection string with the database
+mongoose.connect('mongodb://localhost:27017/piUserDb');
 
 var server = app.listen('3000', function() {
   var port = server.address().port;
